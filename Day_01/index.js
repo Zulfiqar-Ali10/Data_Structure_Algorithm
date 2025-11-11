9d5ce2b9d8f7d80daaffb7ebdf3691db0311040a
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-11-11 16:56:17 +0500

    DSA

--- Day_01/index.js
+++ Day_01/index.js
@@ -1,1 +1,79 @@
-//////////// Start DSA /////////////////////////
+// DSA & Big O Notation //////////////////////////
+
+// function printNumber(n){
+//     for(let i = 0; i <= n; i++){
+//           console.log(i);
+          
+//     }
+// }
+
+// printNumber(5)
+
+
+// function table(number){
+//     for(let i = 1; i <= 10; i++){
+//         console.log(`${number} x ${i} = ${number * i}`);
+//     }
+// }
+// table(5);
+
+
+
+// Example 2: O(n^2) - Quadratic Time Complexity
+
+// function printPairs(n) {
+//   for (let i = 1; i <= n; i++) {
+    
+//     for (let j = 1; j <= n; j++) {
+//       console.log(i, j);
+//     }
+//   }
+// }
+
+// printPairs(3);
+
+////////////////////////////////////////////
+
+// var number = 2;
+
+// if(number % 2 == 0 ){
+//     console.log("Even Number");
+// }else{
+//     console.log("Odd Number");
+// }
+
+
+//////////////////////////////////////////////
+
+
+// function printEvenNumber(number){
+//     for(let i = 1; i <= number; i++){
+//         if(i % 2 == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+
+// printEvenNumber(10)
+
+
+
+
+//////////////////////////////////
+
+// let arr = [1,2,3,4,5,6,7,8,9,10,12,14,16,18, 29];
+
+
+// function printEvenNumbers(arr){
+
+//     for(let i = 1; i <= arr.length; i++){
+//         if(arr[i] % 2 == 0){
+//         console.log(arr[i]);
+//         }
+//     }
+// }
+
+// printEvenNumbers(arr)
+
+
+// DSA & Big O Notation //////////////////////////
