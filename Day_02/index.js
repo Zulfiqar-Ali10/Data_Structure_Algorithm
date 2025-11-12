fbc20513fe09a8454e78ddc523eb5c4d21911e5a
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-11-12 16:32:55 +0500

    DSA

---
+++ Day_02/index.js
@@ -0,0 +1,82 @@
+////////////////////DSA Arrays////////////////////////////////////
+
+// let arr = [10, 5, 20, 8, 15]
+
+// function findMaxMin(arr){
+//     let max = arr[0];
+//     let min = arr[0];
+
+//     for(let i = 1; i < arr.length; i++){
+//         if(arr[i] > max){
+//             max = arr[i];
+//         }
+
+//         if(arr[i] < min){
+//             min = arr[i];
+//         }
+//     }
+
+//     console.log("Maximum:", max);
+//     console.log("Minimum:", min);
+    
+// }
+
+// findMaxMin(arr);
+
+///////////////////////////////////////////////////////////
+
+// let arr = [1,2,3,4,5,6,8,9,10]
+
+// function reverseArray(arr) {
+//     let reversed = [];
+
+//     for(let i = arr.length - 1; i >= 0; i--){
+//         reversed.push(arr[i]);
+//     }
+
+//     return reversed;
+// }
+
+// reverseArray(arr);      
+// console.log("reverseArray", reverseArray(arr));
+
+//////////////////////////////////////////////////////////
+
+// let arr = [1,2,3,4,5,2,4]
+// let unique = [];
+
+// for(let i = 0; i < arr.length; i++){
+//     if(!unique.includes(arr[i])){
+//         unique.push(arr[i]);
+//     }
+// }
+
+// console.log(unique);
+
+
+////////////////////////////////////////////////////////////////////////
+
+// let arr = [1, 2, 2, 3, 4, 4, 5];
+// let unique = new Set(arr);
+// console.log(unique);  ///  ye set object hai is ko array m convert karna parta hai   
+
+
+
+// let arr1 = [1,2,3,4,5,2,4,5,6];
+// let unique1 = [...new Set(arr)];
+// console.log(unique);
+
+
+///////////////////////////////////////////////////////////
+
+// function sumArr(arr){
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++ ) {
+//       sum = sum + arr[i];
+//     }
+//     return sum;
+// }
+
+// console.log(sumArr([1,2,3,4,5]));
+
+////////////////////DSA Arrays////////////////////////////////////
