c3e95afd41605ad9864a17d01e52fdb08f909b24
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-11-13 17:40:33 +0500

    DSA

--- Day_03/index.js
+++ Day_03/index.js
@@ -77,6 +77,85 @@
 
 
 
+//////////////////////////////////////////////////
+
+// function reversedStr(str){
+//     let reversed = "";
+
+//     for(let i = str.length - 1; i >= 0; i--){
+//         reversed += str[i];
+//     }
+
+//     return reversed
+// }
+
+// console.log(reversedStr("Hello"));
+
+
+// function reversed(str){
+//     return str.split("").reverse().join();
+// }
+
+// console.log(reversed("Madam"));
+
+///////////////////////////////////////////////////////
+
+// function subString(str, sub){
+//     return str.includes(sub);
+
+// }
+
+
+// console.log(subString("hello world", "world"));
+
+///////////////////////////////////////////////////////////
+
+// function reverseWords(str){
+//    let words = str.split(" ");
+//     console.log(words);
+   
+//    let reverse = words.reverse();
+//    console.log(reverse);
+
+//    let join = reverse.join();
+   
+    
+//     return join;
+// }
+
+// console.log(reverseWords("I Love Javascript"));
+
+
+///////////////////////////////////////////////////////////////////
+
+// function countSubString(str, sub) {
+//   let count = 0;       // substring kitni baar mili uska counter
+//   let position = 0;    // kaha se search start karna hai
+
+//   while (true) {
+//     // indexOf(sub, start) substring ko dhoondhta hai aur uska index return karta hai
+//     position = str.indexOf(sub, position);
+
+//     // agar nahi mila (indexOf -1 deta hai) to loop se bahar nikal jao
+//     if (position === -1) {
+//       break;
+//     }
+
+//     // mil gaya to count badha do
+//     count++;
+
+//     // next search ke liye ek step aage se dhoondhna start karo
+//     position = position + 1;
+//   }
+
+//   return count; // total baar milne ki ginti return karo
+// }
+
+// console.log(countSubString("lolol", "lo")); // Output: 2
+// console.log(countSubString("aaaaa", "aa")); // Output: 4
+// console.log(countSubString("abaaa", "ab")); // Output: 
+
+
 
 
 //////////////////////////DSA ///////////////////////////////////////////
